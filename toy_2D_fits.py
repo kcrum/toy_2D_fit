9e9a8355987af1bcca7f631d7116826fd02d79ac
Keith Crum <kcrum3@gmail.com>
2014-04-13 23:12:59 -0500

    Added a function to check the minimum expected bin contents of the 1-D and 2-D fits.

--- toy_2D_fits.py
+++ toy_2D_fits.py
@@ -23,20 +23,45 @@ import scipy.stats as st
 # 5) After loop, fit param. dists. with Gaussians and chi^2 curve
 #
 
-def mainloop(nevents1, nevents2, endpoint1=12.0, endpoint2=8.0, lifetime1=260,
-             lifetime2=170, nEbins=4, nTbins=4):
-    maxT = max(lifetime1, lifetime2)
-    maxE = max(endpoint1, endpoint2)
-    energypdfs = [pdfs.ParabolicPDF(endpoint1), pdfs.ParabolicPDF(endpoint2)]
-    timepdfs = [pdfs.TruncatedExponentialPDF(lifetime1, maxT),
-               pdfs.TruncatedExponentialPDF(lifetime2, maxT)]
+def mainloop(nevents0, nevents1, endpoint0 = 12.0, endpoint1 = 8.0, 
+             lifetime0 = 260, lifetime1 = 170, nEbins = 4, nTbins = 4, 
+             minevtsperbin = 20):
+    maxT = max(lifetime0, lifetime1)
+    maxE = max(endpoint0, endpoint1)
+    energypdfs = [pdfs.ParabolicPDF(endpoint0), pdfs.ParabolicPDF(endpoint1)]
+    timepdfs = [pdfs.TruncatedExponentialPDF(lifetime0, maxT),
+               pdfs.TruncatedExponentialPDF(lifetime1, maxT)]
+    # Get vectors of expected fractional bin content.
     energyfracs = [energypdfs[0].binfractionvector(nEbins, (0,maxE)),
                    energypdfs[1].binfractionvector(nEbins, (0,maxE))]
     timefracs = [timepdfs[0].binfractionvector(nTbins, (0,maxT)),
                  timepdfs[1].binfractionvector(nTbins, (0,maxT))]
-    # Take outer product of vectors to get 2-D array. 
+    # Take outer product of vectors to get 2-D array. Rows are time bins, columns
+    # are energy bins. 
     fracs2D = [np.outer(timefracs[0],energyfracs[0]), 
                np.outer(timefracs[1],energyfracs[1])]
+
+    # Make sure no bin has fewer than 'minevtsperbin' events.
+    checkminbins(minevtsperbin, [nevents0,nevents1], energyfracs, timefracs, 
+                 fracs2D)
+
+    
+
+
+# This finds the bins with the fewest expected events in both the 1-D and 2-D 
+# cases.
+def checkminbins(minevtsperbin, nevents, energyfracs, timefracs, twoDfracs):
+    min1Dbin = min(np.min(nevents[0]*energyfracs[0] + nevents[1]*energyfracs[1]),
+                   np.min(nevents[0]*timefracs[0] + nevents[1]*timefracs[1]))    
+    min2Dbin = np.min(nevents[0]*twoDfracs[0] + nevents[1]*twoDfracs[1]) 
+
+    if min1Dbin <= minevtsperbin:
+        print 'Warning: there is a bin in the 1-D fit with %s expected events. Either increase event rate or make binning coarser. Exiting.'
+        sys.exit(1)
+    if min2Dbin <= minevtsperbin:
+        print 'Warning: there is a bin in the 2-D fit with %s expected events. Either increase event rate or make binning coarser. Exiting.'
+        sys.exit(1)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
